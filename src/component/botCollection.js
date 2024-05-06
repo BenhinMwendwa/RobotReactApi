6853ab727d308630a3b9b7b89cb9551a1523376a
Benhin <benhinmwendwa7@gmail.com>
2024-05-07 02:14:49 +0300

    introduced fetch to fetch data

--- src/component/botCollection.js
+++ src/component/botCollection.js
@@ -1,7 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import React from 'react'
+import { useEffect ,useState } from "react";
 
-const botCollection = () => {
+
+const BotCollection = ({ onEnlist }) => {
+    const [bots, setBots] = useState([]);
+  
+    useEffect(() => {
+      fetch('http://localhost:3000/bots')
+        .then(response => response.json())
+        .then(data => setBots(data))
+        .catch(error => console.error('Error fetching bots:', error));
+    }, []);
+  
+    const handleEnlist = (bot) => {
+      onEnlist(bot);
+    };
+  
+
+
   return (
     <div>
       
