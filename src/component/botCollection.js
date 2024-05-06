10bc4709d3ee67899236376be5266a3cd1b96e09
Benhin <benhinmwendwa7@gmail.com>
2024-05-07 02:02:54 +0300

    introduced a hook

---
+++ src/component/botCollection.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import React from 'react'
+
+const botCollection = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default botCollection
