e663817a2e2fe97b5053e8342b5e8c24270bc282
Benhin <benhinmwendwa7@gmail.com>
2024-05-07 03:35:54 +0300

    created a div which i passed the data of the bot

--- src/component/BotData.js
+++ src/component/BotData.js
@@ -1,6 +1,16 @@
 import React from "react";
 const BotData=({bot,onRemove})=>{
     const handleRelease=()=>{// calll the remove function
+       
         onRemove(bot.id)
     }
-}
+    return(
+        <div className='bot-data'>
+            <h3>{bot.name}</h3>
+            <p>{bot.type}</p>
+            <p>{bot.attack}</p>
+            <p>{bot.defense}</p>
+            <p>{bot.speed}</p>
+            <button onClick={handleRelease}>Release</button>
+        </div>
+    )
